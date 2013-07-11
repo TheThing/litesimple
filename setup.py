a49e81a6295b7b35ac17938cd2e791c5f35b540f
Jónatan Nilsson <jonatan@nilsson.is>
2013-07-11 04:35:22 +0000

    Added setup.py for PyPi.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='litesimple',
+    version='0.1.0',
+    description='Simple python ORM micro-framework for sqlite3',
+    author='Jonatan Nilsson',
+    license='WTFPL-2',
+    author_email='jonatan@nilsson.is',
+    url='https://github.com/TheThing/litesimple',
+    long_description=open('readme.md', 'r').read(),
+    packages=[
+        'litesimple',
+    ],
+    requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
